e9b994d856b8cfea2e215d807a324d2c323c367d
Alexandra1503 <78117396+Alexandra1503@users.noreply.github.com>
2021-02-24 17:47:51 -0500

    Add files via upload

---
+++ EXAMEN/CLASES/CLIENTE.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASES
+{
+
+    // : PERSONA (PARA HEREDAR)
+
+   public class CLIENTE : PERSONA
+    {
+
+        private string CATEGORIA;
+        private string CODIGODEPEDIDO;
+
+        public string CATEGORIA1
+        {
+            get
+            {
+                return CATEGORIA;
+            }
+
+            set
+            {
+                CATEGORIA = value;
+            }
+        }
+
+        public string CODIGODEPEDIDO1
+        {
+            get
+            {
+                return CODIGODEPEDIDO;
+            }
+
+            set
+            {
+                CODIGODEPEDIDO = value;
+            }
+        }
+
+        //MÉTODOS
+
+        public void GENERARCODIGODEPEDIDO()
+
+        {
+            this.CODIGODEPEDIDO1 = "C" + this.APPATERNO1.Substring(0,3);
+
+            // Substring(0,3) EXTRAE LA CADENA Y TOMA 3 VALORES 
+        }
+
+    }
+}
---
+++ EXAMEN/CLASES/EMPLEADO.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASES
+{
+    public class EMPLEADO : PERSONA
+    {
+
+        private string TIPO;
+        private string TIPOCONTRATO;
+        private double SUELDO;
+
+          // MÉTODOS
+
+        public string TIPO1
+        {
+            get
+            {
+                return TIPO;
+            }
+
+            set
+            {
+                TIPO = value;
+            }
+        }
+
+        public string TIPOCONTRATO1
+        {
+            get
+            {
+                return TIPOCONTRATO;
+            }
+
+            set
+            {
+                TIPOCONTRATO = value;
+            }
+        }
+
+        public double SUELDO1
+        {
+            get
+            {
+                return SUELDO;
+            }
+
+            set
+            {
+                SUELDO = value;
+            }
+        }
+
+        //MÉTODOS
+
+        public void CALCULARSUELDO (double SUELDOBASE)
+
+        {
+            
+            if (this.TIPO1 == "GERENTE")
+            {
+                if (this.TIPOCONTRATO1 == "NUEVO")
+                {
+                    SUELDO1 = 2 * SUELDOBASE + 500;
+                }
+                else if (this.TIPOCONTRATO1 == "LONGEVO")
+                {
+                    SUELDO1 = 2 * SUELDOBASE + 1500;
+                }
+            }
+            else if (this.TIPO1 == "VENDEDOR")
+            {
+                if (this.TIPOCONTRATO1 == "NUEVO")
+                {
+                    SUELDO1 = SUELDOBASE + 500;
+                }
+                else if (this.TIPOCONTRATO1 == "LONGEVO")
+                {
+                    SUELDO1 = SUELDOBASE + 1500;
+                }
+
+            }else
+            {
+                this.SUELDO1 = 0;
+            }
+        }
+//.
+
+    }
+}
